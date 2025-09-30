059250a832b88d384e6944591b1f3a56a3b9e41e
username <wecp-dummy@gmail.com>
2025-09-30 06:51:07 +0000

    EventPlannerController.java

--- server/src/main/java/com/wecp/eventmanagementsystem/service/EventService.java
+++ server/src/main/java/com/wecp/eventmanagementsystem/service/EventService.java
@@ -9,8 +9,47 @@ import org.springframework.stereotype.Service;
 import javax.persistence.EntityNotFoundException;
 import java.util.List;
 
-
+@Service
 public class EventService {
+    @Autowired
+    private EventRepository eventRepository;
 
+    public List<Event> getAllEvents(){
+        return eventRepository.findAll();
+    }
+    public Event getEventsById(Long id){
+        Event e = eventRepository.findById(id).orElse(null);
+        if(e == null){
+            throw new EntityNotFoundException("Event not found!");
+        }
+        else{
+            return e;
+        }
+    }
+    public Event createEvent(Event event){
+        return eventRepository.save(event);
+    }
+    public Event updateEventById(Event event,Long id){
+        Event existingEvent =eventRepository.findById(id).orElseThrow(EntityNotFoundException::new);
+        existingEvent.setTitle(event.getTitle());
+        existingEvent.setDateTime(event.getDateTime());
+        existingEvent.setDescription(event.getDescription());
+        existingEvent.setLocation(event.getLocation());
+        existingEvent.setStatus(event.getStatus());
+        existingEvent.setAllocations(event.getAllocations());
+        return eventRepository.save(existingEvent);
+    }
+    
+    public void deleteEvent(Long eventId){
+        eventRepository.deleteById(eventId);
+    }
+
+    public List<Event> getAllEventByTitle(String title){
+        return eventRepository.findByTitle(title);
+    }
+
+    public Event getAllEventByTitles(String title){
+        return eventRepository.findEventByTitle(title);
+    }
 
 }
