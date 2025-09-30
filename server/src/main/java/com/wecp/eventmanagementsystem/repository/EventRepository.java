f7d6d52d19590c5ef23d305a76f509a7740fc29f
username <wecp-dummy@gmail.com>
2025-09-30 05:08:46 +0000

    repositories and resource service

--- server/src/main/java/com/wecp/eventmanagementsystem/repository/EventRepository.java
+++ server/src/main/java/com/wecp/eventmanagementsystem/repository/EventRepository.java
@@ -1,10 +1,15 @@
 package com.wecp.eventmanagementsystem.repository;
 
 import com.wecp.eventmanagementsystem.entity.Event;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-
-public interface EventRepository {
+@Repository
+public interface EventRepository extends JpaRepository<Event,Long>{
     // extend jpa repository and add custom method if needed
+    List<Event> findByTitle(String title);
+    public Event findEventByTitle(String title);
 }
